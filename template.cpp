0051839cc5ac5622f8ea0f8542da63082faef57d
Rafael <capitao.rafa@gmail.com>
2016-06-08 22:05:39 +0000

    update 1

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    cout<< fixed <<setprecision(2);
+    int n,c=0,c2=0;
+    double x;
+    cin>>x;
+    while(c<100){
+        cout<< "N["<<c<<"] = "<<x<<endl;
+    x=x/2;c++;}
+}
